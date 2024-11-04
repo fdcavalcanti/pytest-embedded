dd19870bbc2151cf89f63c4e010abd3de22bdb70
Filipe Cavalcanti <filipe.cavalcanti@espressif.com>
2024-11-04 10:03:15 -0300

    Added serial module of NuttX

--- pytest-embedded-nuttx/pytest_embedded_nuttx/__init__.py
+++ pytest-embedded-nuttx/pytest_embedded_nuttx/__init__.py
@@ -1,8 +1,21 @@
 """Make pytest-embedded plugin work with Arduino."""
 
+import importlib
+
+from pytest_embedded.utils import lazy_load
+
 from .app import NuttxApp
 
-__all__ = ['NuttxApp']
+__getattr__ = lazy_load(
+    importlib.import_module(__name__),
+    {
+        'NuttxApp': NuttxApp,
+    },
+    {
+        'NuttxSerial': '.serial',
+    },
+)
 
+__all__ = ['NuttxApp', 'NuttxSerial']
 
 __version__ = '1.11.8'
